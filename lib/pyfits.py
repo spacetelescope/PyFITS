13e13c1d859ecc7e07fdd010828f5d732149c89a
hsu <hsu@ed100bfc-0583-0410-97f2-c26b58777a21>
2002-03-21 17:02:29 +0000

    Change data comparison (with a string value) from "==" to "is" to make it working under Python 2.2.
    git-svn-id: http://svn6.assembla.com/svn/pyfits/trunk@40 ed100bfc-0583-0410-97f2-c26b58777a21

--- lib/pyfits.py
+++ lib/pyfits.py
@@ -1,34 +1,4 @@
 #!/usr/bin/env python2.0
-"""
-Copyright (C) 2001 Association of Universities for Research in Astronomy (AURA)
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-
-    2. Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-
-    3. The name of AURA and its representatives may not be used to
-      endorse or promote products derived from this software without
-      specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY AURA ``AS IS'' AND ANY EXPRESS OR IMPLIED
-WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL AURA BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-DAMAGE.
-"""
 
 """A module for reading and writing FITS files.
 
@@ -54,7 +24,7 @@ import numarray as num
 import chararray
 import recarray as rec
 
-version = '0.6.2 (Feb 12, 2002)'
+version = '0.6.2.1 (Mar 21, 2002)'
 
 # Public variables
 blockLen = 2880         # the FITS block size
@@ -842,7 +812,7 @@ class ImageBaseHDU:
             # Make a "copy" (not just a view) of the input header, since it
             # may get modified.
             # the data is still a "view" (for now)
-            if data != "delayed":
+            if data is not "delayed":
                 self.header = Header(header.ascard.copy())
 
             # if the file is read the first time, no need to copy
@@ -858,7 +828,7 @@ class ImageBaseHDU:
         self.scale = self.header.get('BSCALE', 1)
         self.autoscale = (self.zero != 0) or (self.scale != 1)
 
-        if (data == "delayed"): return
+        if (data is "delayed"): return
 
         old_naxis = self.header['NAXIS']
 
