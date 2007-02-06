f17a17ba41b85e1ec329a8a89a5b8c4a8a4e2a58
Christopher Hanley <chanley@gmail.com>
2007-02-06 18:14:22 +0000

    Fixed use of main __doc__ in pyfits namesapce.
    git-svn-id: http://svn6.assembla.com/svn/pyfits/trunk@263 ed100bfc-0583-0410-97f2-c26b58777a21

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,19 +1,18 @@
-"""
-This is the configuration file for the pyfits namespace.  This is needed
-because we have the option of using either a numarray or numpy version
-of pyfits.
+#This is the configuration file for the pyfits namespace.  This is needed
+#because we have the option of using either a numarray or numpy version
+#of pyfits.
 
-This option is controlled by the NUMERIX environment variable.  Set NUMERIX 
-to 'numarray' for the numarray version of pyfits.  Set NUMERIX to 'numpy'
-for the numpy version of pyfits.
+#This option is controlled by the NUMERIX environment variable.  Set NUMERIX 
+#to 'numarray' for the numarray version of pyfits.  Set NUMERIX to 'numpy'
+#for the numpy version of pyfits.
 
-If only one array package is installed, that package's version of pyfits
-will be imported.  If both packages are installed the NUMERIX value is
-used to decide between the packages.  If no NUMERIX value is set then 
-the numarray version of pyfits will be imported.
+#If only one array package is installed, that package's version of pyfits
+#will be imported.  If both packages are installed the NUMERIX value is
+#used to decide between the packages.  If no NUMERIX value is set then 
+#the numarray version of pyfits will be imported.
 
-Anything else is an exception.
-"""
+#Anything else is an exception.
+
 import os
 
 __version__ = '1.1b4'
@@ -46,10 +45,12 @@ if (numpystatus and numarraystatus):
     if numerix == 'numpy':
         from NP_pyfits import *
         import NP_pyfits as core
+        __doc__ = NP_pyfits.__doc__
     else:
         from NA_pyfits import *
         import NA_pyfits as core
-
+        __doc__ = NA_pyfits.__doc__
+        
 elif (numpystatus):
     # if only numpy is installed use the numpy version of pyfits
     from NP_pyfits import *
