36ed1d476b9ef144e879df14856455ecf5d49109
Christopher Hanley <chanley@gmail.com>
2007-05-21 20:19:56 +0000

    basic example of unittest
    git-svn-id: http://svn6.assembla.com/svn/pyfits/trunk@301 ed100bfc-0583-0410-97f2-c26b58777a21

---
+++ test/testPyfitsImage.py
@@ -0,0 +1,16 @@
+import unittest
+
+class TestPyfitsImageFunctions(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    
+    def testopen(self):
+        ...
+        
+    def testclose(self):
+        ...
+    
+if __name__ == '__main__':
+    unittest.main()
+    
