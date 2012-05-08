spacetelescope/PyFITS

commit 747af33d9efef695d19aeb04b4219fba5059bef9

This will fix #147, while not breaking any existing regression tests.  I'm a little nervous about this one though--I worry that there are still more corner cases just around the corner.  Even with the addition of some new tests, I'm not sure that this will always get things right.  If nothing else, at some point I need to make a chart of the logic used when writing a FITS file.  There are a few different paths, and it's worth explaining what they are and why they're needed.
git-svn-id: http://svn6.assembla.com/svn/pyfits/trunk@1497 ed100bfc-0583-0410-97f2-c26b58777a21

lib/pyfits/hdu/base.py
@@ -99,6 +99,7 @@ class _BaseHDU(object):
         self._hdrLoc = None
         self._datLoc = None
         self._datSpan = None
+        self._new = True
         self.name = ''
 
     @property
@@ -284,7 +285,7 @@ class _BaseHDU(object):
 
     # TODO: Rework checksum handling so that it's not necessary to add a
     # checksum argument here
-    def _prewriteto(self, checksum=False):
+    def _prewriteto(self, checksum=False, inplace=False):
         # If the data is unsigned int 16, 32, or 64 add BSCALE/BZERO
         # cards to header
         if self._data_loaded and self.data is not None and \
@@ -297,23 +298,30 @@ class _BaseHDU(object):
                              after='BSCALE')
 
         # Handle checksum
-        if 'CHECKSUM' in self._header:
-            del self._header['CHECKSUM']
 
-        if 'DATASUM' in self._header:
-            del self._header['DATASUM']
+        # If the data is loaded it isn't necessarily 'modified', but we have no
+        # way of knowing for sure
+        modified = self._header._modified or self._data_loaded
 
-        if checksum == 'datasum':
-            self.add_datasum()
-        elif checksum == 'nonstandard_datasum':
-            self.add_datasum(blocking='nonstandard')
-        elif checksum == 'test':
-            self.add_datasum(self._datasum_comment)
-            self.add_checksum(self._checksum_comment, True)
-        elif checksum == 'nonstandard':
-            self.add_checksum(blocking='nonstandard')
-        elif checksum:
-            self.add_checksum(blocking='standard')
+        if modified or (not checksum and not inplace):
+            if 'CHECKSUM' in self._header:
+                del self._header['CHECKSUM']
+
+            if 'DATASUM' in self._header:
+                del self._header['DATASUM']
+
+        if checksum and (modified or self._new):
+            if checksum == 'datasum':
+                self.add_datasum()
+            elif checksum == 'nonstandard_datasum':
+                self.add_datasum(blocking='nonstandard')
+            elif checksum == 'test':
+                self.add_datasum(self._datasum_comment)
+                self.add_checksum(self._checksum_comment, True)
+            elif checksum == 'nonstandard':
+                self.add_checksum(blocking='nonstandard')
+            elif checksum:
+                self.add_checksum(blocking='standard')
 
     def _postwriteto(self):
         # If data is unsigned integer 16, 32 or 64, remove the
@@ -399,15 +407,18 @@ class _BaseHDU(object):
     # Though right now this is an internal private method (though still used by
     # HDUList, eventually the plan is to have this be moved into writeto()
     # somehow...
-    def _writeto(self, fileobj, inplace=False):
+    def _writeto(self, fileobj, inplace=False, copy=False):
         # For now fileobj is assumed to be a _File object
-        if not inplace:
+        if not inplace or self._new:
             return ((self._writeheader(fileobj)[0],) +
                     self._writedata(fileobj))
 
         if self.header._modified:
             self._file.seek(self._hdrLoc)
             self._writeheader(fileobj)
+        elif copy:
+            self._file.seek(self._hdrLoc)
+            fileobj.write(self._file.read(self._datLoc - self._hdrLoc))
         if self._data_loaded:
             if self.data is not None:
                 # Seek through the array's bases for an memmap'd array; we
@@ -420,6 +431,9 @@ class _BaseHDU(object):
                 else:
                     self._file.seek(self._datLoc)
                     self._writedata(fileobj)
+        elif copy:
+            self._file.seek(self._datLoc)
+            fileobj.write(self._file.read(self._datSpan))
         return (self._hdrLoc, self._datLoc,
                 self._datSpan + _pad_length(self._datSpan))
 

lib/pyfits/tests/test_hdulist.py
@@ -524,3 +524,37 @@ class TestHDUListFunctions(PyfitsTestCase):
 
         with pyfits.open(self.temp('temp.fits')) as hdul:
             assert_true((hdul[0].data == data).all())
+
+    def test_update_resized_header(self):
+        """
+        Test saving updates to a file where the header is one block smaller
+        than before, and in the case where the heade ris one block larger than
+        before.
+        """
+
+        data = np.arange(100)
+        hdu = pyfits.PrimaryHDU(data=data)
+        idx = 1
+        while len(str(hdu.header)) <= 2880:
+            hdu.header['TEST%d' % idx] = idx
+            idx += 1
+        orig_header = hdu.header.copy()
+        hdu.writeto(self.temp('temp.fits'))
+
+        with pyfits.open(self.temp('temp.fits'), mode='update') as hdul:
+            while len(str(hdul[0].header)) > 2880:
+                del hdul[0].header[-1]
+
+        with pyfits.open(self.temp('temp.fits')) as hdul:
+            assert_equal(hdul[0].header, orig_header[:-1])
+            assert_true((hdul[0].data == data).all())
+
+        with pyfits.open(self.temp('temp.fits'), mode='update') as hdul:
+            idx = 101
+            while len(str(hdul[0].header)) <= 2880 * 2:
+                hdul[0].header['TEST%d' % idx] = idx
+                idx += 1
+
+        with pyfits.open(self.temp('temp.fits')) as hdul:
+            assert_equal(hdul[0].header[:-37], orig_header[:-1])
+            assert_true((hdul[0].data == data).all())

lib/pyfits/hdu/compressed.py
@@ -1691,11 +1691,12 @@ class CompImageHDU(BinTableHDU):
 
     # TODO: Fix this class so that it doesn't actually inherit from
     # BinTableHDU, but instead has an internal BinTableHDU reference
-    def _prewriteto(self, checksum=False):
+    def _prewriteto(self, checksum=False, inplace=False):
         self.updateCompressedData()
         # Doesn't call the super's _prewriteto, since it calls
         # self.data._scale_back(), which is meaningless here.
-        return ExtensionHDU._prewriteto(self, checksum=checksum)
+        return ExtensionHDU._prewriteto(self, checksum=checksum,
+                                        inplace=inplace)
 
     def _writeheader(self, fileobj):
         """

lib/pyfits/hdu/hdulist.py
@@ -544,7 +544,7 @@ class HDUList(list, _Verify):
                         extver = ''
 
                 # only append HDU's which are "new"
-                if not hasattr(hdu, '_new') or hdu._new:
+                if hdu._new:
                     # only output the checksum if flagged to do so
                     if hasattr(hdu, '_output_checksum'):
                         checksum = hdu._output_checksum
@@ -807,7 +807,7 @@ class HDUList(list, _Verify):
                 checksum = hdu._output_checksum
             else:
                 checksum = False
-            hdu._prewriteto(checksum=checksum)
+            hdu._prewriteto(checksum=checksum, inplace=True)
 
         try:
             self._wasresized()
@@ -851,7 +851,7 @@ class HDUList(list, _Verify):
             hdulist = self.fromfile(new_file, mode='append')
 
             for hdu in self:
-                offsets = hdu._writeto(hdulist.__file)
+                offsets = hdu._writeto(hdulist.__file, inplace=True, copy=True)
                 hdu._hdrLoc, hdu._datLoc, hdu._datSpan = offsets
 
             if sys.platform.startswith('win'):
@@ -924,7 +924,7 @@ class HDUList(list, _Verify):
             ffo.seek(0)
 
             for hdu in hdulist:
-                offsets = hdu._writeto(ffo)
+                offsets = hdu._writeto(ffo, inplace=True, copy=True)
                 hdu._hdrLoc, hdu._datLoc, hdu._datSpan = offsets
 
             # Close the temporary file and delete it.

lib/pyfits/hdu/image.py
@@ -444,9 +444,11 @@ class _ImageBaseHDU(_ValidHDU):
 
         return super(_ImageBaseHDU, self)._verify(option)
 
-    def _prewriteto(self, checksum=False):
+    def _prewriteto(self, checksum=False, inplace=False):
         self.update_header()
-        return super(_ImageBaseHDU, self)._prewriteto(checksum)
+        if not inplace and not self._data_loaded:
+            self._update_header_scale_info()
+        return super(_ImageBaseHDU, self)._prewriteto(checksum, inplace)
 
     def _writedata_internal(self, fileobj):
         size = 0
@@ -483,14 +485,6 @@ class _ImageBaseHDU(_ValidHDU):
             size += output.size * output.itemsize
 
         return size
-
-    def _writeto(self, fileobj, inplace=False):
-        if not inplace and not self._data_loaded:
-            # Normally this is done when the data is loaded, but since the data
-            # is not loaded yet we need to update the header appropriately
-            # before writing it
-            self._update_header_scale_info()
-        return super(_ImageBaseHDU, self)._writeto(fileobj, inplace=inplace)
 
     def _dtype_for_bitpix(self):
         """

lib/pyfits/hdu/table.py
@@ -263,7 +263,7 @@ class _TableBaseHDU(ExtensionHDU, _TableLikeHDU):
         return new_table(self.columns, header=self._header,
                          tbtype=self.columns._tbtype)
 
-    def _prewriteto(self, checksum=False):
+    def _prewriteto(self, checksum=False, inplace=False):
         if self._data_loaded and self.data is not None:
             self.data._scale_back()
             # check TFIELDS and NAXIS2
@@ -285,7 +285,7 @@ class _TableBaseHDU(ExtensionHDU, _TableLikeHDU):
                     max = self.data.field(idx).max
                     format = _FormatP(format, repeat=format.repeat, max=max)
                     self._header['TFORM' + str(idx + 1)] = format.tform
-        return super(_TableBaseHDU, self)._prewriteto(checksum)
+        return super(_TableBaseHDU, self)._prewriteto(checksum, inplace)
 
     def _verify(self, option='warn'):
         """

commit d564a296281065614d402760f3709649ba3449dd

Fixed a bug that caused some regression tests to fail. When resizing a file with memmap=True, the data was just flushed to the original file rather than being copied to the new file.  This is a regression introduced in r1497.  Updated one of the tests so that it catches this bug.
git-svn-id: http://svn6.assembla.com/svn/pyfits/trunk@1521 ed100bfc-0583-0410-97f2-c26b58777a21

lib/pyfits/hdu/base.py
@@ -424,7 +424,13 @@ class _BaseHDU(object):
                 # Seek through the array's bases for an memmap'd array; we
                 # can't rely on the _File object to give us this info since the
                 # user may have replaced the previous mmap'd array
-                memmap_array = _get_array_memmap(self.data)
+                if copy:
+                    # Of course, if we're copying the data to a new file we
+                    # don't care about flushing the original mmap; instead just
+                    # read it into the new file
+                    memmap_array = None
+                else:
+                    memmap_array = _get_array_memmap(self.data)
 
                 if memmap_array is not None:
                     memmap_array.flush()

lib/pyfits/tests/test_hdulist.py
@@ -565,7 +565,10 @@ class TestHDUListFunctions(PyfitsTestCase):
             while len(str(hdul[0].header)) <= 2880 * 2:
                 hdul[0].header['TEST%d' % idx] = idx
                 idx += 1
+            # Touch something in the data too so that it has to be rewritten
+            hdul[0].data[0] = 27
 
         with pyfits.open(self.temp('temp.fits')) as hdul:
             assert_equal(hdul[0].header[:-37], orig_header[:-1])
-            assert_true((hdul[0].data == data).all())
+            assert_equal(hdul[0].data[0], 27)
+            assert_true((hdul[0].data[1:] == data[1:]).all())