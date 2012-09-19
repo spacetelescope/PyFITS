2244ba6df93fa136be5f0b822fce7de9d6ed7e40
Erik Bray <erik.m.bray@gmail.com>
2012-09-19 18:38:29 +0000

    Setting up dev version; see r1779 for the equivalent commit on trunk
    git-svn-id: http://svn6.assembla.com/svn/pyfits/branches/3.1-stable@1817 ed100bfc-0583-0410-97f2-c26b58777a21

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,9 +99,9 @@ copyright = ', '.join([copyright_year] + authors)
 # built documents.
 #
 # The short X.Y version.
-version = '3.1'
+version = '3.1.1'
 # The full version, including alpha/beta/rc tags.
-release = '3.1'
+release = '3.1.1'
 
 # !!!!!
 
