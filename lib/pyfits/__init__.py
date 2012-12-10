59eaeb99aa19c7acdefe25d457a2e068ea68a077
Erik Bray <erik.m.bray@gmail.com>
2012-12-10 20:14:13 +0000

    Add a hacky fix to work around a bug in nose when running 'setup.py nosetests'; TODO: Submit bug report and maybe a fix to nose.
    git-svn-id: http://svn6.assembla.com/svn/pyfits/trunk@1946 ed100bfc-0583-0410-97f2-c26b58777a21

--- lib/pyfits/__init__.py
+++ lib/pyfits/__init__.py
@@ -1,6 +1,21 @@
-#This is the configuration file for the pyfits namespace.
+# This is the configuration file for the pyfits namespace.
 
-from __future__ import division # confidence high
+from __future__ import division  # confidence high
+
+# Delete any existing pyfits subpackages from sys.modules;
+# This is a hack specifically to work with ./setup.py nosetests:
+# When running setup.py, python gets imported from the source checkout during
+# the build process. ./setup.py nosetests, however, runs the 'build' command
+# and then tries to import pyfits from out of the 'build/' directory.  Since
+# pyfits has already been partially imported from 'lib/' this leads to
+# confusion.
+# nose already attempts to handle this case, but it only works on single
+# modules; it fails to delete any *submodules* that are already in sys.modules.
+# This should be considered a bug in nose.
+import sys
+if 'pyfits' in sys.modules:
+    for kw in [kw for kw in sys.modules if kw.startswith('pyfits.')]:
+        del sys.modules[kw]
 
 try:
     from pyfits.version import __version__
@@ -9,6 +24,7 @@ except:
 
 # Import the pyfits core module.
 import pyfits.core
+
 from pyfits.core import *
 from pyfits.util import *
 
