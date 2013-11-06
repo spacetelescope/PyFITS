d826c70e2015b263148d28c08c017e9e67a67df8
Erik Bray <erik.m.bray@gmail.com>
2013-11-06 21:12:17 +0000

    Allow assignment from np.void objects, which are returned for the rows of multi-field ndarrays (i.e. not recarrays)
    git-svn-id: http://svn6.assembla.com/svn/pyfits/trunk@2324 ed100bfc-0583-0410-97f2-c26b58777a21

--- lib/pyfits/fitsrec.py
+++ lib/pyfits/fitsrec.py
@@ -303,7 +303,7 @@ class FITS_rec(np.recarray):
         if isinstance(value, FITS_record):
             for idx in range(self._nfields):
                 self.field(self.names[idx])[row] = value.field(self.names[idx])
-        elif isinstance(value, (tuple, list)):
+        elif isinstance(value, (tuple, list, np.void)):
             if self._nfields == len(value):
                 for idx in range(self._nfields):
                     self.field(idx)[row] = value[idx]
