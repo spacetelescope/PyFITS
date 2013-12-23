spacetelescope/PyFITS

commit ca3c3a7b56d7c82441e1071a658f3e0537a97d48

A number of "low hanging fruit" micro-optimizations in header handling. This won't make a noticeable difference for much anyone, but it should shave a couple seconds off in cases like spacetelescope/PyFITS#46
I won't list every change, but most of them are things like re-ordering
if/elif statements so that the most common cases are handled earliest.

Some of the more significant changes are in handling of RVKCs,
unsurprisingly.  I made some slight tweaks so that we can check for them
faster in the most common cases.  I also added an additional
_rvkc_indices data structure in the Header class to help keep track of
them.

I'm not crazy about the tradeoff here between efficiency and code
complexity, as the additional keyword index does add some complexity,
but it makes failed keyword loopups, which are actually quite frequent
when initializing an HDU and trying to determin its type.  So I think
it's worth it.

lib/pyfits/header.py
@@ -105,6 +105,12 @@ class Header(object):
             yield card.keyword
 
     def __contains__(self, keyword):
+        if keyword in self._keyword_indices or keyword in self._rvkc_indices:
+            # For the most common case (single, standard form keyword lookup)
+            # this will work and is an O(1) check.  If it fails that doesn't
+            # guarantee absense, just that we have to perform the full set of
+            # checks in self._cardindex
+            return True
         try:
             self._cardindex(keyword)
         except (KeyError, IndexError):
@@ -127,8 +133,7 @@ class Header(object):
         else:
             keyword = key
         card = self._cards[self._cardindex(key)]
-        if (card.field_specifier is not None and
-                keyword == card.keyword.split('.', 1)[0]):
+        if card.field_specifier is not None and keyword == card.rawkeyword:
             # This is RVKC; if only the top-level keyword was specified return
             # the raw value, not the parsed out float value
             return card.rawvalue
@@ -203,7 +208,11 @@ class Header(object):
         elif isinstance(key, basestring):
             # delete ALL cards with the same keyword name
             key = Card.normalize_keyword(key)
+            indices = self._keyword_indices
             if key not in self._keyword_indices:
+                indices = self._rvkc_indices
+
+            if key not in indices:
                 if _is_pyfits_internal():
                     # All internal code is designed to assume that this will
                     # raise a KeyError, so go ahead and do so
@@ -219,18 +228,27 @@ class Header(object):
                     'caught and handled when deleting non-existent keywords.' %
                     key, DeprecationWarning)
                 return
-            for idx in reversed(self._keyword_indices[key]):
+            for idx in reversed(indices[key]):
                 # Have to copy the indices list since it will be modified below
                 del self[idx]
             return
 
         idx = self._cardindex(key)
-        keyword = self._cards[idx].keyword
+        card = self._cards[idx]
+        keyword = card.keyword
         del self._cards[idx]
         indices = self._keyword_indices[keyword]
         indices.remove(idx)
         if not indices:
             del self._keyword_indices[keyword]
+
+        # Also update RVKC indices if necessary :/
+        if card.field_specifier is not None:
+            indices = self._rvkc_indices[card.rawkeyword]
+            indices.remove(idx)
+            if not indices:
+                del self._rvkc_indices[card.rawkeyword]
+
 
         # We also need to update all other indices
         self._updateindices(idx, increment=False)
@@ -624,6 +642,7 @@ class Header(object):
 
         self._cards = []
         self._keyword_indices = defaultdict(list)
+        self._rvkc_indices = defaultdict(list)
 
     def copy(self, strip=False):
         """
@@ -1130,6 +1149,8 @@ class Header(object):
 
         keyword = Card.normalize_keyword(card.keyword)
         self._keyword_indices[keyword].append(idx)
+        if card.field_specifier is not None:
+            self._rvkc_indices[card.rawkeyword].append(idx)
 
         if not end:
             # If the appended card was a commentary card, and it was appended
@@ -1199,15 +1220,16 @@ class Header(object):
         else:
             first = None
 
-        # This copy is used to check for duplicates in this header prior to the
-        # extend, while not counting duplicates in the header being extended
-        # from (see ticket #156)
-        orig = self[:]
+        # We don't immediately modify the header, because first we need to sift
+        # out any duplicates in the new header prior to adding them to the
+        # existing header, but while *allowing* duplicates from the header
+        # being exteded from (see ticket #156)
+        extend_cards = []
 
         for idx, card in enumerate(temp.cards):
             keyword = card.keyword
             if keyword not in Card._commentary_keywords:
-                if unique and not update and keyword in orig:
+                if unique and not update and keyword in self:
                     continue
                 elif update:
                     if idx == 0 and update_first:
@@ -1220,30 +1242,28 @@ class Header(object):
                             self.insert(0, card)
                         else:
                             self[keyword] = (card.value, card.comment)
-                    elif keyword in orig:
+                    elif keyword in self:
                         self[keyword] = (card.value, card.comment)
                     else:
-                        self.append(card, useblanks=useblanks, bottom=bottom,
-                                    end=end)
+                        extend_cards.append(card)
                 else:
-                    self.append(card, useblanks=useblanks, bottom=bottom,
-                                end=end)
+                    extend_cards.append(card)
             else:
-                if unique or update and keyword in orig:
+                if unique or update and keyword in self:
                     if str(card) == BLANK_CARD:
-                        self.append(card, useblanks=useblanks, bottom=bottom,
-                                    end=end)
+                        extend_cards.append(card)
                         continue
 
-                    for value in orig[keyword]:
+                    for value in self[keyword]:
                         if value == card.value:
                             break
                     else:
-                        self.append(card, useblanks=useblanks, bottom=bottom,
-                                    end=end)
+                        extend_cards.append(card)
                 else:
-                    self.append(card, useblanks=useblanks, bottom=bottom,
-                                end=end)
+                    extend_cards.append(card)
+
+        for card in extend_cards:
+            self.append(card, useblanks=useblanks, bottom=bottom, end=end)
 
     def count(self, keyword):
         """
@@ -1365,6 +1385,13 @@ class Header(object):
                     'A %r keyword already exists in this header.  Inserting '
                     'duplicate keyword.' % keyword)
             self._keyword_indices[keyword].sort()
+
+        if card.field_specifier is not None:
+            # Update the index of RVKC as well
+            rvkc_indices = self._rvkc_indices[card.rawkeyword]
+            rvkc_indices.append(idx)
+            rvkc_indices.sort()
+
 
         if useblanks:
             self._useblanks(len(str(card)) // Card.length)
@@ -1525,8 +1552,12 @@ class Header(object):
     def _cardindex(self, key):
         """Returns an index into the ._cards list given a valid lookup key."""
 
-        if isinstance(key, slice):
-            return key
+        # This used to just set key = (key, 0) and then go on to act as if the
+        # user passed in a tuple, but it's much more common to just be given a
+        # string as the key, so optimize more for that case
+        if isinstance(key, basestring):
+            keyword = key
+            n = 0
         elif isinstance(key, int):
             # If < 0, determine the actual index
             if key < 0:
@@ -1534,48 +1565,40 @@ class Header(object):
             if key < 0 or key >= len(self._cards):
                 raise IndexError('Header index out of range.')
             return key
-
-        if isinstance(key, basestring):
-            key = (key, 0)
-
-        if isinstance(key, tuple):
+        elif isinstance(key, slice):
+            return key
+        elif isinstance(key, tuple):
             if (len(key) != 2 or not isinstance(key[0], basestring) or
                     not isinstance(key[1], int)):
                 raise ValueError(
                     'Tuple indices must be 2-tuples consisting of a '
                     'keyword string and an integer index.')
             keyword, n = key
-            keyword = Card.normalize_keyword(keyword)
-            # Returns the index into _cards for the n-th card with the given
-            # keyword (where n is 0-based)
-            if keyword and keyword not in self._keyword_indices:
-                if len(keyword) > KEYWORD_LENGTH or '.' in keyword:
-                    raise KeyError("Keyword %r not found." % keyword)
-                # Great--now we have to check if there's a RVKC that starts
-                # with the given keyword, making failed lookups fairly
-                # expensive
-                # TODO: Find a way to make this more efficient; perhaps a set
-                # of RVKCs in the header or somesuch.
-                keyword = keyword + '.'
-                found = 0
-                for idx, card in enumerate(self._cards):
-                    if (card.field_specifier and
-                            card.keyword.startswith(keyword)):
-                        if found == n:
-                            return idx
-                        found += 1
-                else:
-                    raise KeyError("Keyword %r not found." % keyword[:-1])
-            try:
-                return self._keyword_indices[keyword][n]
-            except IndexError:
-                raise IndexError('There are only %d %r cards in the header.' %
-                                 (len(self._keyword_indices[keyword]),
-                                  keyword))
         else:
             raise ValueError(
                 'Header indices must be either a string, a 2-tuple, or '
                 'an integer.')
+
+        keyword = Card.normalize_keyword(keyword)
+        # Returns the index into _cards for the n-th card with the given
+        # keyword (where n is 0-based)
+        indices = self._keyword_indices.get(keyword, None)
+
+        if keyword and not indices:
+            if len(keyword) > KEYWORD_LENGTH or '.' in keyword:
+                raise KeyError("Keyword %r not found." % keyword)
+            else:
+                # Maybe it's a RVKC?
+                indices = self._rvkc_indices.get(keyword, None)
+
+        if not indices:
+            raise KeyError("Keyword %r not found." % keyword)
+
+        try:
+            return indices[n]
+        except IndexError:
+            raise IndexError('There are only %d %r cards in the header.' %
+                             (len(indices), keyword))
 
     def _relativeinsert(self, card, before=None, after=None, replace=False):
         """
@@ -1924,7 +1947,7 @@ class _CardAccessor(object):
     """
     This is a generic class for wrapping a Header in such a way that you can
     use the header's slice/filtering capabilities to return a subset of cards
-    and do something of them.
+    and do something with them.
 
     This is sort of the opposite notion of the old CardList class--whereas
     Header used to use CardList to get lists of cards, this uses Header to get
@@ -1939,7 +1962,10 @@ class _CardAccessor(object):
         return '\n'.join(repr(c) for c in self._header._cards)
 
     def __len__(self):
-        return len([c for c in self])
+        return len(self._header._cards)
+
+    def __iter__(self):
+        return iter(self._header._cards)
 
     def __eq__(self, other):
         if isiterable(other):
@@ -1987,6 +2013,10 @@ class _HeaderComments(_CardAccessor):
     of values.
     """
 
+    def __iter__(self):
+        for card in self._header._cards:
+            yield card.comment
+
     def __repr__(self):
         """Returns a simple list of all keywords and their comments."""
 
@@ -2004,6 +2034,7 @@ class _HeaderComments(_CardAccessor):
 
         item = super(_HeaderComments, self).__getitem__(item)
         if isinstance(item, _HeaderComments):
+            # The item key was a slice
             return item
         return item.comment
 
@@ -2025,11 +2056,25 @@ class _HeaderComments(_CardAccessor):
 
 
 class _HeaderCommentaryCards(_CardAccessor):
+    """
+    This is used to return a list-like sequence over all the values in the
+    header for a given commentary keyword, such as HISTORY.
+    """
+
     def __init__(self, header, keyword=''):
         super(_HeaderCommentaryCards, self).__init__(header)
         self._keyword = keyword
         self._count = self._header.count(self._keyword)
         self._indices = slice(self._count).indices(self._count)
+
+    # __len__ and __iter__ need to be overridden from the base class due to the
+    # different approach this class has to take for slicing
+    def __len__(self):
+        return len(xrange(*self._indices))
+
+    def __iter__(self):
+        for idx in xrange(*self._indices):
+            yield self._header[(self._keyword, idx)]
 
     def __repr__(self):
         return '\n'.join(self)

lib/pyfits/card.py
@@ -380,7 +380,7 @@ class Card(_Verify):
     _rvkc_field_specifier_val = (r'(?P<keyword>%s): (?P<val>%s)' %
                                  (_rvkc_field_specifier_s, _numr_FSC))
     _rvkc_keyword_val = r'\'(?P<rawval>%s)\'' % _rvkc_field_specifier_val
-    _rvkc_keyword_val_comm = (r' +%s *(/ *(?P<comm>[ -~]*))?$' %
+    _rvkc_keyword_val_comm = (r' *%s *(/ *(?P<comm>[ -~]*))?$' %
                               _rvkc_keyword_val)
 
     _rvkc_field_specifier_val_RE = re.compile(_rvkc_field_specifier_val + '$')
@@ -397,7 +397,7 @@ class Card(_Verify):
     # (ex "'AXIS.1: 1' / a comment")
     _rvkc_keyword_val_comm_RE = re.compile(_rvkc_keyword_val_comm)
 
-    _commentary_keywords = ['', 'COMMENT', 'HISTORY', 'END']
+    _commentary_keywords = set(['', 'COMMENT', 'HISTORY', 'END'])
 
     # The default value indicator; may be changed if required by a convention
     # (namely HIERARCH cards)
@@ -411,7 +411,7 @@ class Card(_Verify):
         self._keyword = None
         self._value = None
         self._comment = None
-        self._rawvalue = None
+
         self._image = None
 
         # This attribute is set to False when creating the card from a card
@@ -428,7 +428,12 @@ class Card(_Verify):
         self._invalid = False
 
         self._field_specifier = None
-        if not self._check_if_rvkc(keyword, value):
+        # These are used primarily only by RVKCs
+        self._rawkeyword = None
+        self._rawvalue = None
+
+        if not (keyword is not None and value is not None and
+                self._check_if_rvkc(keyword, value)):
             # If _check_if_rvkc passes, it will handle setting the keyword and
             # value
             if keyword is not None:
@@ -604,6 +609,12 @@ class Card(_Verify):
                                  'keyword cards')
 
     @property
+    def rawkeyword(self):
+        if self._rawkeyword is None and self.keyword:
+            self._rawkeyword = self.keyword
+        return self._rawkeyword
+
+    @property
     def rawvalue(self):
         if self._rawvalue is None and self.value:
             self._rawvalue = self.value
@@ -743,6 +754,13 @@ class Card(_Verify):
             The converted string
         """
 
+        # Test first for the most common case: a standard FITS keyword provided
+        # in standard all-caps
+        if (len(keyword) <= KEYWORD_LENGTH and
+                cls._keywd_FSC_RE.match(keyword)):
+            return keyword
+
+        # Test if this is a record-valued keyword
         match = cls._rvkc_keyword_name_RE.match(keyword)
 
         if match:
@@ -754,6 +772,7 @@ class Card(_Verify):
             # "HIERARCH HIERARCH", but shame on you if you do that.
             return keyword[9:].strip()
         else:
+            # A normal FITS keyword, but provided in non-standard case
             return keyword.strip().upper()
 
     def _check_if_rvkc(self, *args):
@@ -782,22 +801,7 @@ class Card(_Verify):
             return False
 
         if len(args) == 1:
-            image = args[0]
-            eq_idx = image.find('=')
-            if eq_idx < 0 or eq_idx > 9:
-                return False
-            keyword, rest = image.split('=', 1)
-            if keyword in self._commentary_keywords:
-                return False
-            match = self._rvkc_keyword_val_comm_RE.match(rest)
-            if match:
-                field_specifier = match.group('keyword')
-                self._keyword = '.'.join((keyword.strip().upper(),
-                                          field_specifier))
-                self._field_specifier = field_specifier
-                self._value = _int_or_float(match.group('val'))
-                self._rawvalue = match.group('rawval')
-                return True
+            self._check_if_rvkc_image(*args)
         elif len(args) == 2:
             keyword, value = args
             if not isinstance(keyword, basestring):
@@ -812,36 +816,88 @@ class Card(_Verify):
                 self._field_specifier = field_specifier
                 self._value = value
                 return True
-            if isinstance(value, basestring):
+
+            # Testing for ': ' is a quick way to avoid running the full regular
+            # expression, speeding this up for the majority of cases
+            if isinstance(value, basestring) and value.find(': ') > 0:
                 match = self._rvkc_field_specifier_val_RE.match(value)
                 if match and self._keywd_FSC_RE.match(keyword):
-                    field_specifier = match.group('keyword')
-                    self._keyword = '.'.join((keyword.upper(),
-                                              field_specifier))
-                    self._field_specifier = field_specifier
-                    self._value = _int_or_float(match.group('val'))
-                    self._rawvalue = value
+                    self._init_rvkc(keyword, match.group('keyword'), value,
+                                    match.group('val'))
                     return True
 
-    def _parse_keyword(self):
-        if self._check_if_rvkc(self._image):
-            return self._keyword
+    def _check_if_rvkc_image(self, *args):
+        """
+        Implements `Card._check_if_rvkc` for the case of an unparsed card
+        image.  If given one argment this is the full intact image.  If given
+        two arguments the card has already been split between keyword and
+        value+comment at the standard value indicator '= '.
+        """
 
+        if len(args) == 1:
+            image = args[0]
+            eq_idx = image.find(VALUE_INDICATOR)
+            if eq_idx < 0 or eq_idx > 9:
+                return False
+            keyword = image[:eq_idx]
+            rest = image[eq_idx + len(VALUE_INDICATOR):]
+        else:
+            keyword, rest = args
+
+        rest = rest.lstrip()
+
+        # This test allows us to skip running the full regular expression for
+        # the majority of cards that do not contain strings or that definitely
+        # do not contain RVKC field-specifiers; it's very much a
+        # micro-optimization but it does make a measurable difference
+        if not rest or rest[0] != "'" or rest.find(': ') < 2:
+            return False
+
+        match = self._rvkc_keyword_val_comm_RE.match(rest)
+        if match:
+            self._init_rvkc(keyword, match.group('keyword'),
+                            match.group('rawval'), match.group('val'))
+            return True
+
+    def _init_rvkc(self, keyword, field_specifier, field, value):
+        """
+        Sort of addendum to Card.__init__ to set the appropriate internal
+        attributes if the card was determined to be a RVKC.
+        """
+
+        keyword_upper = keyword.upper()
+        self._keyword = '.'.join((keyword_upper, field_specifier))
+        self._rawkeyword = keyword_upper
+        self._field_specifier = field_specifier
+        self._value = _int_or_float(value)
+        self._rawvalue = field
+
+    def _parse_keyword(self):
         keyword = self._image[:KEYWORD_LENGTH].strip()
         keyword_upper = keyword.upper()
-        value_indicator = self._image.find(VALUE_INDICATOR)
+        val_ind_idx = self._image.find(VALUE_INDICATOR)
 
-        special = self._commentary_keywords + ['CONTINUE']
+        special = self._commentary_keywords
 
-        if keyword_upper in special or 0 <= value_indicator <= KEYWORD_LENGTH:
+        if (0 <= val_ind_idx <= KEYWORD_LENGTH or keyword_upper in special or
+                keyword_upper == 'CONTINUE'):
             # The value indicator should appear in byte 8, but we are flexible
             # and allow this to be fixed
-            if value_indicator >= 0:
-                keyword = keyword[:value_indicator]
-                keyword_upper = keyword_upper[:value_indicator]
+            if val_ind_idx >= 0:
+                keyword = keyword[:val_ind_idx]
+                rest = self._image[val_ind_idx + len(VALUE_INDICATOR):]
+
+                # So far this looks like a standard FITS keyword; check whether
+                # the value represents a RVKC; if so then we pass things off to
+                # the RVKC parser
+                if self._check_if_rvkc_image(keyword, rest):
+                    return self._keyword
+
+                keyword_upper = keyword_upper[:val_ind_idx]
 
             if keyword_upper != keyword:
                 self._modified = True
+
             return keyword_upper
         elif (keyword_upper == 'HIERARCH' and self._image[8] == ' ' and
               HIERARCH_VALUE_INDICATOR in self._image):
@@ -962,7 +1018,7 @@ class Card(_Verify):
         else:
             image = self.image
 
-        if self.keyword in self._commentary_keywords + ['CONTINUE']:
+        if self.keyword in self._commentary_keywords.union(['CONTINUE']):
             keyword, valuecomment = image.split(' ', 1)
         else:
             try:

lib/pyfits/hdu/compressed.py
@@ -72,6 +72,7 @@ class CompImageHeader(Header):
             image_header = Header()
         self._cards = image_header._cards
         self._keyword_indices = image_header._keyword_indices
+        self._rvkc_indices = image_header._rvkc_indices
         self._modified = image_header._modified
         self._table_header = table_header
 

commit c8fbcd8478e50ea5712dbcc488a2bde87cd7d356

Fixed a regression introduced in ca3c3a7b56d7c82441e1071a658f3e0537a97d48 where looping over and deleting record-valued keyword cards could, in some cases, result in an exception depending on what order they were deleted in
git-svn-id: http://svn6.assembla.com/svn/pyfits/trunk@2440 ed100bfc-0583-0410-97f2-c26b58777a21

lib/pyfits/header.py
@@ -1671,10 +1671,11 @@ class Header(object):
 
         increment = 1 if increment else -1
 
-        for indices in self._keyword_indices.itervalues():
-            for jdx, keyword_index in enumerate(indices):
-                if keyword_index >= idx:
-                    indices[jdx] += increment
+        for index_sets in (self._keyword_indices, self._rvkc_indices):
+            for indices in index_sets.itervalues():
+                for jdx, keyword_index in enumerate(indices):
+                    if keyword_index >= idx:
+                        indices[jdx] += increment
 
     def _countblanks(self):
         """Returns the number of blank cards at the end of the Header."""

lib/pyfits/tests/test_header.py
@@ -2074,6 +2074,15 @@ class TestRecordValuedKeywordCards(PyfitsTestCase):
         assert self._test_header.keys()[1] == 'DP1.AXIS.2'
         assert self._test_header[1] == 2
 
+        # Perform a subsequent delete to make sure all the index mappings were
+        # updated
+        del self._test_header['DP1.AXIS.2']
+        assert len(self._test_header) == 6
+        assert self._test_header.keys()[0] == 'DP1.NAXIS'
+        assert self._test_header[0] == 2
+        assert self._test_header.keys()[1] == 'DP1.NAUX'
+        assert self._test_header[1] == 2
+
     def test_pattern_matching_keys(self):
         """Test the keyword filter strings with RVKCs."""
 