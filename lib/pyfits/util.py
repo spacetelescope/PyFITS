fb4da3ff24f6b1f3f307c86f91cd84bbbe0d8eab
Erik Bray <erik.m.bray@gmail.com>
2014-01-28 00:30:24 +0000

    Fix a TODO item that is necessary for fileobj_is_binary to return the correct result for a StringIO object in Python 3
    Conflicts:
    
    	lib/pyfits/util.py
    
    git-svn-id: http://svn6.assembla.com/svn/pyfits/trunk@2507 ed100bfc-0583-0410-97f2-c26b58777a21

--- lib/pyfits/util.py
+++ lib/pyfits/util.py
@@ -13,6 +13,11 @@ import textwrap
 import threading
 import warnings
 
+try:
+    import io
+except ImportError:
+    io = None
+
 import numpy as np
 
 from .extern.six import (PY3, iteritems, string_types, integer_types,
@@ -684,8 +689,9 @@ def fileobj_is_binary(f):
     if hasattr(f, 'binary'):
         return f.binary
 
-    # TODO: In Python 3 it might be more reliable to check if the fileobj is a
-    # text reader or a binary reader
+    if io is not None and isinstance(f, io.TextIOBase):
+        return False
+
     mode = fileobj_mode(f)
     if mode:
         return 'b' in mode
