5e4260f56ceeaa90c552080e4713ee97d5a5c26b
Erik Bray <erik.m.bray@gmail.com>
2014-05-07 23:05:29 +0000

    Add izip_longest implementation for python 2.5
    git-svn-id: http://svn6.assembla.com/svn/pyfits/branches/3.2-stable@2704 ed100bfc-0583-0410-97f2-c26b58777a21

--- lib/pyfits/util.py
+++ lib/pyfits/util.py
@@ -1005,3 +1005,28 @@ if sys.version_info[:2] < (2, 6):
 
             return cls_ns[property_name]
     __builtin__.property = property
+
+
+    # Provide an implementation of izip_longest
+    class ZipExhausted(Exception):
+        pass
+
+    def izip_longest(*args, **kwds):
+        # izip_longest('ABCD', 'xy', fillvalue='-') --> Ax By C- D-
+        fillvalue = kwds.get('fillvalue')
+        counter = [len(args) - 1]
+        def sentinel():
+            if not counter[0]:
+                raise ZipExhausted
+            counter[0] -= 1
+            yield fillvalue
+        fillers = itertools.repeat(fillvalue)
+        iterators = [itertools.chain(it, sentinel(), fillers) for it in args]
+        try:
+            while iterators:
+                yield tuple(map(next, iterators))
+        except ZipExhausted:
+            pass
+
+    from .extern import six
+    six.moves.zip_longest = izip_longest
