77190a51981bf1bfdc8403a881c36a0b15150734
Erik Bray <erik.m.bray@gmail.com>
2014-05-14 16:47:09 +0000

    Preparing release 3.2.3
    git-svn-id: http://svn6.assembla.com/svn/pyfits/branches/3.2-stable@2729 ed100bfc-0583-0410-97f2-c26b58777a21

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,9 +99,9 @@ copyright = ', '.join([copyright_year] + authors)
 # built documents.
 #
 # The short X.Y version.
-version = '3.2.3.dev'
+version = '3.2.3'
 # The full version, including alpha/beta/rc tags.
-release = '3.2.3.dev'
+release = '3.2.3'
 
 # !!!!!
 
