1e979d738d009ff42692cc2b26131ef0c5e4d68c
Erik Bray <erik.m.bray@gmail.com>
2014-05-14 16:23:34 +0000

    Back to development: 3.1.7
    git-svn-id: http://svn6.assembla.com/svn/pyfits/branches/3.1-stable@2724 ed100bfc-0583-0410-97f2-c26b58777a21

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,9 +99,9 @@ copyright = ', '.join([copyright_year] + authors)
 # built documents.
 #
 # The short X.Y version.
-version = '3.1.6'
+version = '3.1.7.dev'
 # The full version, including alpha/beta/rc tags.
-release = '3.1.6'
+release = '3.1.7.dev'
 
 # !!!!!
 
