da7d55542c307e750c933236b13d0eda9b325f63
Erik Bray <erik.m.bray@gmail.com>
2014-07-17 18:20:45 +0000

    Back to development: 3.4
    git-svn-id: http://svn6.assembla.com/svn/pyfits/trunk@2793 ed100bfc-0583-0410-97f2-c26b58777a21

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,9 +99,9 @@ copyright = ', '.join([copyright_year] + authors)
 # built documents.
 #
 # The short X.Y version.
-version = '3.3'
+version = '3.4.dev'
 # The full version, including alpha/beta/rc tags.
-release = '3.3'
+release = '3.4.dev'
 
 # !!!!!
 
