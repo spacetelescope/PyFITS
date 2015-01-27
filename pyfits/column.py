5cf4029b023dae58c6d59b4dea271df5451d1e08
Erik M. Bray <embray@stsci.edu>
2015-01-27 16:41:46 -0500

    The format argument, despite being a keyword argument, is *not* optional

--- pyfits/column.py
+++ pyfits/column.py
@@ -365,7 +365,7 @@ class Column(object):
         name : str, optional
             column name, corresponding to ``TTYPE`` keyword
 
-        format : str, optional
+        format : str
             column format, corresponding to ``TFORM`` keyword
 
         unit : str, optional
