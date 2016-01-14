astropy/astropy

Small modifications to enable keyword retrieval

astropy/io/fits/scripts/fitsheader.py
@@ -19,7 +19,15 @@ Example uses of fitsheader:
 
     $ fitsheader --ext "SCI,2" filename.fits
 
-4. Print the headers of all fits files in a directory::
+4. Print the value of a specific header keyword only::
+
+    $ fitsheader --keyword BITPIX filename.fits
+
+4. Print the header of several specific keywords::
+
+    $ fitsheader --keyword BITPIX,NAXIS1,NAXIS2 filename.fits
+
+6. Print the headers of all fits files in a directory::
 
     $ fitsheader *.fits
 
@@ -56,15 +64,14 @@ class HeaderFormatter(object):
     compressed : boolean, optional
         show the header describing the compression (for CompImageHDU's only)
     """
-    def __init__(self, filename, compressed=False, keyword=None):
+    def __init__(self, filename, compressed=False):
         try:
             self.hdulist = fits.open(filename)
         except IOError as e:
             raise FormattingException(str(e))
         self.compressed = compressed
-        self.keyword = keyword
 
-    def parse(self, extension=None):
+    def parse(self, extension=None, keywords=None):
         """Returns the FITS file header(s) in a readable format.
 
         Parameters
@@ -72,6 +79,10 @@ class HeaderFormatter(object):
         extension : int or str, optional
             Format only a specific HDU, identified by its number or its name.
             The name is the "EXTNAME" or "EXTNAME,EXTVER" string.
+
+        keywords : str, optional
+            Comma-delimited string of keywords for which the value(s) should be
+            returned. If not specified, then the entire header is returned.
 
         Returns
         -------
@@ -88,15 +99,38 @@ class HeaderFormatter(object):
                 # The user can specify "EXTNAME" or "EXTNAME,EXTVER" as well
                 parts = extension.split(',')
                 if len(parts) > 1:
-                    # We use str() below to avoid issue #2184
                     extname = ','.join(parts[0:-1])
                     extver = int(parts[-1])
                     hdukeys = [(extname, extver)]
                 else:
                     hdukeys = [extension]
 
-        # Having established which HDUs are wanted, we can now format these
-        return self._format_hdulist(hdukeys)
+        # Having established which HDUs the user wants, we now format these:
+        result = []
+        for i, hdukey in enumerate(hdukeys):
+            if keywords:  # Are specific keywords requested?
+                for kw in keywords[0].split(','):
+                    try:
+                        result.append('{0}\n'.format(
+                                      self._get_header(hdukey)[kw]
+                                      ))
+                    except KeyError as e:  # Keyword does not exist
+                        log.warning('{filename} (HDU {hdukey}): '
+                                    'Keyword {kw} not found.'.format(
+                                        filename=self.hdulist.filename(),
+                                        hdukey=hdukey,
+                                        kw=kw))
+                        result.append('\n')  # Show a blank instead of a value
+            else:  # Print the entire header instead of specific keywords
+                if i > 0:  # Separate different HDUs by a blank line
+                    result.append('\n')
+                prettyheader = self._get_header(hdukey) \
+                                   .tostring(sep='\n', padding=False)
+                result.append('# HDU {hdukey} in {filename}:\n{prettyheader}\n'
+                              .format(hdukey=hdukey,
+                                      filename=self.hdulist.filename(),
+                                      prettyheader=prettyheader))
+        return ''.join(result)
 
     def _get_header(self, hdukey):
         """Returns the `astropy.io.fits.header.Header` object for the HDU."""
@@ -114,26 +148,6 @@ class HeaderFormatter(object):
             raise FormattingException('{0}: {1}'.format(
                                       self.hdulist.filename(), str(e)))
 
-    def _format_hdulist(self, hdukeys):
-        """Returns the formatted version of the header; the important bit."""
-        text = []
-        for i, key in enumerate(hdukeys):
-            if self.keyword:  # True if only specific keywords must be shown
-                for kw in self.keyword:
-                    try:
-                        text.append('{0}\n'.format(self._get_header(key)[kw]))
-                    except KeyError as e:  # Keyword does not exist
-                        text.append('\n')
-            else:  # Print the entire header
-                if i > 0:  # Separate different HDUs by a blank line
-                    text.append('\n')
-                text.append('# HDU {key} in {filename}:\n{header}\n'.format(
-                            key=key,
-                            filename=self.hdulist.filename(),
-                            header=self._get_header(key)
-                                       .tostring(sep='\n', padding=False)))
-        return ''.join(text)
-
 
 def main(args=None):
     from astropy.utils.compat import argparse
@@ -143,27 +157,25 @@ def main(args=None):
                      'All HDU extensions are shown by default. '
                      'In the case of a compressed image, '
                      'the decompressed header is shown.'))
+    parser.add_argument('-e', '--ext', metavar='HDU',
+                        help='specify the HDU extension by name or number')
+    parser.add_argument('-k', '--keyword', nargs=1,
+                        help='retrieve the value for a specific keyword, '
+                        'or for a comma-delimited list of keywords')
     parser.add_argument('-c', '--compressed', action='store_true',
                         help='for compressed image data, '
                              'show the true header which describes '
                              'the compression rather than the data')
-    parser.add_argument('-e', '--ext', metavar='hdu',
-                        help='specify the HDU extension number or name')
-    parser.add_argument('-k', '--keyword',
-                        metavar='keyword', nargs='+',
-                        help='show only the value'
-                        ' of the specified keyword(s)')
     parser.add_argument('filename', nargs='+',
-                        help='path to one or more FITS files to display')
+                        help='path to one or more FITS files')
     args = parser.parse_args(args)
 
     try:
         for i, filename in enumerate(args.filename):
             if i > 0 and not args.keyword:
-                print()  # Prints a newline for clarity
-            print(HeaderFormatter(filename,
-                                  args.compressed,
-                                  args.keyword).parse(args.ext), end='')
+                print()  # newline between different headers
+            print(HeaderFormatter(filename, args.compressed)
+                  .parse(args.ext, args.keyword), end='')
     except FormattingException as e:
         log.error(e)
     except IOError as e:

spacetelescope/PyFITS

pyfits/scripts/fitsheader.py
@@ -19,7 +19,15 @@ Example uses of fitsheader:
 
     $ fitsheader --ext "SCI,2" filename.fits
 
-4. Print the headers of all fits files in a directory::
+4. Print the value of a specific header keyword only::
+
+    $ fitsheader --keyword BITPIX filename.fits
+
+4. Print the header of several specific keywords::
+
+    $ fitsheader --keyword BITPIX,NAXIS1,NAXIS2 filename.fits
+
+6. Print the headers of all fits files in a directory::
 
     $ fitsheader *.fits
 
@@ -59,15 +67,14 @@ class HeaderFormatter(object):
     compressed : boolean, optional
         show the header describing the compression (for CompImageHDU's only)
     """
-    def __init__(self, filename, compressed=False, keyword=None):
+    def __init__(self, filename, compressed=False):
         try:
             self.hdulist = pyfits.open(filename)
         except IOError as e:
             raise FormattingException(str(e))
         self.compressed = compressed
-        self.keyword = keyword
 
-    def parse(self, extension=None):
+    def parse(self, extension=None, keywords=None):
         """Returns the FITS file header(s) in a readable format.
 
         Parameters
@@ -75,6 +82,10 @@ class HeaderFormatter(object):
         extension : int or str, optional
             Format only a specific HDU, identified by its number or its name.
             The name is the "EXTNAME" or "EXTNAME,EXTVER" string.
+
+        keywords : str, optional
+            Comma-delimited string of keywords for which the value(s) should be
+            returned. If not specified, then the entire header is returned.
 
         Returns
         -------
@@ -91,15 +102,38 @@ class HeaderFormatter(object):
                 # The user can specify "EXTNAME" or "EXTNAME,EXTVER" as well
                 parts = extension.split(',')
                 if len(parts) > 1:
-                    # We use str() below to avoid issue #2184
                     extname = ','.join(parts[0:-1])
                     extver = int(parts[-1])
                     hdukeys = [(extname, extver)]
                 else:
                     hdukeys = [extension]
 
-        # Having established which HDUs are wanted, we can now format these
-        return self._format_hdulist(hdukeys)
+        # Having established which HDUs the user wants, we now format these:
+        result = []
+        for i, hdukey in enumerate(hdukeys):
+            if keywords:  # Are specific keywords requested?
+                for kw in keywords[0].split(','):
+                    try:
+                        result.append('{0}\n'.format(
+                                      self._get_header(hdukey)[kw]
+                                      ))
+                    except KeyError as e:  # Keyword does not exist
+                        log.warning('{filename} (HDU {hdukey}): '
+                                    'Keyword {kw} not found.'.format(
+                                        filename=self.hdulist.filename(),
+                                        hdukey=hdukey,
+                                        kw=kw))
+                        result.append('\n')  # Show a blank instead of a value
+            else:  # Print the entire header instead of specific keywords
+                if i > 0:  # Separate different HDUs by a blank line
+                    result.append('\n')
+                prettyheader = self._get_header(hdukey) \
+                                   .tostring(sep='\n', padding=False)
+                result.append('# HDU {hdukey} in {filename}:\n{prettyheader}\n'
+                              .format(hdukey=hdukey,
+                                      filename=self.hdulist.filename(),
+                                      prettyheader=prettyheader))
+        return ''.join(result)
 
     def _get_header(self, hdukey):
         """Returns the `pyfits.header.Header` object for the HDU."""
@@ -117,26 +151,6 @@ class HeaderFormatter(object):
             raise FormattingException('{0}: {1}'.format(
                                       self.hdulist.filename(), str(e)))
 
-    def _format_hdulist(self, hdukeys):
-        """Returns the formatted version of the header; the important bit."""
-        text = []
-        for i, key in enumerate(hdukeys):
-            if self.keyword:  # True if only specific keywords must be shown
-                for kw in self.keyword:
-                    try:
-                        text.append('{0}\n'.format(self._get_header(key)[kw]))
-                    except KeyError as e:  # Keyword does not exist
-                        text.append('\n')
-            else:  # Print the entire header
-                if i > 0:  # Separate different HDUs by a blank line
-                    text.append('\n')
-                text.append('# HDU {key} in {filename}:\n{header}\n'.format(
-                            key=key,
-                            filename=self.hdulist.filename(),
-                            header=self._get_header(key)
-                                       .tostring(sep='\n', padding=False)))
-        return ''.join(text)
-
 
 def main(args=None):
     parser = argparse.OptionParser(
@@ -144,26 +158,24 @@ def main(args=None):
                      'All HDU extensions are shown by default. '
                      'In the case of a compressed image, '
                      'the decompressed header is shown.'))
+    parser.add_option('-e', '--ext', metavar='HDU',
+                      help='specify the HDU extension number or name')
+    parser.add_option('-k', '--keyword', nargs=1,
+                      help='show only the value of the specified keyword(s)')
     parser.add_option('-c', '--compressed', action='store_true',
                       help='for compressed image data, '
                            'show the true header which describes '
                            'the compression rather than the data')
-    parser.add_option('-e', '--ext', metavar='hdu',
-                      help='specify the HDU extension number or name')
-    parser.add_option('-k', '--keyword',
-                      metavar='keyword', nargs='+',
-                      help='show only the value of the specified keyword(s)')
     parser.add_option('filename', nargs='+',
-                      help='path to one or more FITS files to display')
+                      help='path to one or more FITS files')
     args = parser.parse_args(args)
 
     try:
         for i, filename in enumerate(args.filename):
             if i > 0 and not args.keyword:
-                print()  # Prints a newline for clarity
-            print(HeaderFormatter(filename,
-                                  args.compressed,
-                                  args.keyword).parse(args.ext), end='')
+                print()  # newline between different headers
+            print(HeaderFormatter(filename, args.compressed)
+                  .parse(args.ext, args.keyword), end='')
     except FormattingException as e:
         log.error(e)
     except IOError as e: