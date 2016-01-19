0xdev7/Astropy

Fix typo in scheme comparison; update tests.

astropy/io/fits/tests/test_core.py
@@ -572,7 +572,7 @@ class TestFileFunctions(FitsTestCase):
         try:
             fits.open(self.temp('foobar.fits'))
         except IOError as e:
-            assert 'File does not exist' in str(e)
+            assert 'No such file or directory' in str(e)
         except:
             raise
 

spacetelescope/PyFITS

pyfits/tests/test_core.py
@@ -560,7 +560,7 @@ class TestFileFunctions(PyfitsTestCase):
         try:
             fits.open(self.temp('foobar.fits'))
         except IOError as exc:
-            assert 'File does not exist' in str(exc)
+            assert 'No such file or directory' in str(exc)
         except:
             raise
 