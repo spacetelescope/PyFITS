0xdev7/Astropy

Change use of .__file attributes to just ._file; this was always inconvenient for debugging, and is not really representative of how double-underscore attriubtes are supposed to work in Python anyway.

astropy/io/fits/file.py
@@ -88,7 +88,7 @@ class _File(object):
         memmap = True if memmap is None else memmap
 
         if fileobj is None:
-            self.__file = None
+            self._file = None
             self.closed = False
             self.binary = True
             self.mode = mode
@@ -133,7 +133,7 @@ class _File(object):
         self.readonly = False
         self.writeonly = False
 
-        # Initialize the internal self.__file object
+        # Initialize the internal self._file object
         if _is_random_access_file_backed(fileobj):
             self._open_fileobj(fileobj, mode, clobber)
         elif isinstance(fileobj, string_types):
@@ -141,7 +141,7 @@ class _File(object):
         else:
             self._open_filelike(fileobj, mode, clobber)
 
-        self.fileobj_mode = fileobj_mode(self.__file)
+        self.fileobj_mode = fileobj_mode(self._file)
 
         if isinstance(fileobj, (_astropy_gzip.GzipFile, _system_gzip.GzipFile)):
             self.compression = 'gzip'
@@ -159,18 +159,18 @@ class _File(object):
         # For 'ab+' mode, the pointer is at the end after the open in
         # Linux, but is at the beginning in Solaris.
         if (mode == 'ostream' or self.compression or
-            not hasattr(self.__file, 'seek')):
+            not hasattr(self._file, 'seek')):
             # For output stream start with a truncated file.
             # For compressed files we can't really guess at the size
             self.size = 0
         else:
-            pos = self.__file.tell()
-            self.__file.seek(0, 2)
-            self.size = self.__file.tell()
-            self.__file.seek(pos)
+            pos = self._file.tell()
+            self._file.seek(0, 2)
+            self.size = self._file.tell()
+            self._file.seek(pos)
 
         if self.memmap:
-            if not isfile(self.__file):
+            if not isfile(self._file):
                 self.memmap = False
             elif not self.readonly and not self._test_mmap():
                 # Test mmap.flush--see
@@ -179,7 +179,7 @@ class _File(object):
 
     def __repr__(self):
         return '<%s.%s %s>' % (self.__module__, self.__class__.__name__,
-                               self.__file)
+                               self._file)
 
     # Support the 'with' statement
     def __enter__(self):
@@ -191,13 +191,13 @@ class _File(object):
     def readable(self):
         if self.writeonly:
             return False
-        return isreadable(self.__file)
+        return isreadable(self._file)
 
     def read(self, size=None):
-        if not hasattr(self.__file, 'read'):
+        if not hasattr(self._file, 'read'):
             raise EOFError
         try:
-            return self.__file.read(size)
+            return self._file.read(size)
         except IOError:
             # On some versions of Python, it appears, GzipFile will raise an
             # IOError if you try to read past its end (as opposed to just
@@ -216,7 +216,7 @@ class _File(object):
         it's provided for compatibility.
         """
 
-        if not hasattr(self.__file, 'read'):
+        if not hasattr(self._file, 'read'):
             raise EOFError
 
         if not isinstance(dtype, np.dtype):
@@ -246,26 +246,26 @@ class _File(object):
             shape = (1,)
 
         if self.memmap:
-            return Memmap(self.__file, offset=offset,
+            return Memmap(self._file, offset=offset,
                           mode=MEMMAP_MODES[self.mode], dtype=dtype,
                           shape=shape).view(np.ndarray)
         else:
             count = reduce(lambda x, y: x * y, shape)
-            pos = self.__file.tell()
-            self.__file.seek(offset)
-            data = _array_from_file(self.__file, dtype, count, '')
+            pos = self._file.tell()
+            self._file.seek(offset)
+            data = _array_from_file(self._file, dtype, count, '')
             data.shape = shape
-            self.__file.seek(pos)
+            self._file.seek(pos)
             return data
 
     def writable(self):
         if self.readonly:
             return False
-        return iswritable(self.__file)
+        return iswritable(self._file)
 
     def write(self, string):
-        if hasattr(self.__file, 'write'):
-            _write_string(self.__file, string)
+        if hasattr(self._file, 'write'):
+            _write_string(self._file, string)
 
     def writearray(self, array):
         """
@@ -275,51 +275,51 @@ class _File(object):
         the file on disk reflects the data written.
         """
 
-        if hasattr(self.__file, 'write'):
-            _array_to_file(array, self.__file)
+        if hasattr(self._file, 'write'):
+            _array_to_file(array, self._file)
 
     def flush(self):
-        if hasattr(self.__file, 'flush'):
-            self.__file.flush()
+        if hasattr(self._file, 'flush'):
+            self._file.flush()
 
     def seek(self, offset, whence=0):
         # In newer Python versions, GzipFiles support the whence argument, but
         # I don't think it was added until 2.6; instead of assuming it's
         # present, we implement our own support for it here
-        if not hasattr(self.__file, 'seek'):
+        if not hasattr(self._file, 'seek'):
             return
-        if isinstance(self.__file, (_astropy_gzip.GzipFile, _system_gzip.GzipFile)):
+        if isinstance(self._file, (_astropy_gzip.GzipFile, _system_gzip.GzipFile)):
             if whence:
                 if whence == 1:
-                    offset = self.__file.offset + offset
+                    offset = self._file.offset + offset
                 else:
                     raise ValueError('Seek from end not supported')
-            self.__file.seek(offset)
+            self._file.seek(offset)
         else:
-            self.__file.seek(offset, whence)
+            self._file.seek(offset, whence)
 
-        pos = self.__file.tell()
+        pos = self._file.tell()
         if self.size and pos > self.size:
             warnings.warn('File may have been truncated: actual file length '
                           '(%i) is smaller than the expected size (%i)' %
                           (self.size, pos), AstropyUserWarning)
 
     def tell(self):
-        if not hasattr(self.__file, 'tell'):
+        if not hasattr(self._file, 'tell'):
             raise EOFError
-        return self.__file.tell()
+        return self._file.tell()
 
     def truncate(self, size=None):
-        if hasattr(self.__file, 'truncate'):
-            self.__file.truncate(size)
+        if hasattr(self._file, 'truncate'):
+            self._file.truncate(size)
 
     def close(self):
         """
         Close the 'physical' FITS file.
         """
 
-        if hasattr(self.__file, 'close'):
-            self.__file.close()
+        if hasattr(self._file, 'close'):
+            self._file.close()
 
         self.closed = True
 
@@ -366,16 +366,16 @@ class _File(object):
                 raise ValueError(
                     "Mode argument '%s' does not match mode of the input "
                     "file (%s)." % (mode, fmode))
-            self.__file = fileobj
+            self._file = fileobj
         elif isfile(fileobj):
-            self.__file = fileobj_open(self.name, PYFITS_MODES[mode])
+            self._file = fileobj_open(self.name, PYFITS_MODES[mode])
         else:
-            self.__file = _astropy_gzip.open(self.name, PYFITS_MODES[mode])
+            self._file = _astropy_gzip.open(self.name, PYFITS_MODES[mode])
 
         if fmode == 'ab+':
             # Return to the beginning of the file--in Python 3 when opening in
             # append mode the file pointer is at the end of the file
-            self.__file.seek(0)
+            self._file.seek(0)
 
     def _open_filelike(self, fileobj, mode, clobber):
         """Open a FITS file from a file-like object, i.e. one that has
@@ -383,7 +383,7 @@ class _File(object):
         """
 
         self.file_like = True
-        self.__file = fileobj
+        self._file = fileobj
 
         if fileobj_closed(fileobj):
             raise IOError("Cannot read from/write to a closed file-like "
@@ -391,15 +391,15 @@ class _File(object):
 
         if isinstance(fileobj, zipfile.ZipFile):
             self._open_zipfile(fileobj, mode)
-            self.__file.seek(0)
+            self._file.seek(0)
             # We can bypass any additional checks at this point since now
-            # self.__file points to the temp file extracted from the zip
+            # self._file points to the temp file extracted from the zip
             return
 
         # If there is not seek or tell methods then set the mode to
         # output streaming.
-        if (not hasattr(self.__file, 'seek') or
-            not hasattr(self.__file, 'tell')):
+        if (not hasattr(self._file, 'seek') or
+            not hasattr(self._file, 'tell')):
             self.mode = mode = 'ostream'
 
         if mode == 'ostream':
@@ -407,13 +407,13 @@ class _File(object):
 
         # Any "writeable" mode requires a write() method on the file object
         if (self.mode in ('update', 'append', 'ostream') and
-            not hasattr(self.__file, 'write')):
+            not hasattr(self._file, 'write')):
             raise IOError("File-like object does not have a 'write' "
                           "method, required for mode '%s'."
                           % self.mode)
 
         # Any mode except for 'ostream' requires readability
-        if self.mode != 'ostream' and not hasattr(self.__file, 'read'):
+        if self.mode != 'ostream' and not hasattr(self._file, 'read'):
             raise IOError("File-like object does not have a 'read' "
                           "method, required for mode %r."
                           % self.mode)
@@ -434,15 +434,15 @@ class _File(object):
 
         if ext == '.gz' or magic.startswith(GZIP_MAGIC):
             # Handle gzip files
-            self.__file = _astropy_gzip.open(self.name, PYFITS_MODES[mode])
+            self._file = _astropy_gzip.open(self.name, PYFITS_MODES[mode])
             self.compression = 'gzip'
         elif ext == '.zip' or magic.startswith(PKZIP_MAGIC):
             # Handle zip files
             self._open_zipfile(self.name, mode)
         else:
-            self.__file = fileobj_open(self.name, PYFITS_MODES[mode])
+            self._file = fileobj_open(self.name, PYFITS_MODES[mode])
             # Make certain we're back at the beginning of the file
-        self.__file.seek(0)
+        self._file.seek(0)
 
     def _test_mmap(self):
         """Tests that mmap, and specifically mmap.flush works.  This may
@@ -508,8 +508,8 @@ class _File(object):
         if len(namelist) != 1:
             raise IOError(
               "Zip files with multiple members are not supported.")
-        self.__file = tempfile.NamedTemporaryFile(suffix='.fits')
-        self.__file.write(zfile.read(namelist[0]))
+        self._file = tempfile.NamedTemporaryFile(suffix='.fits')
+        self._file.write(zfile.read(namelist[0]))
 
         if close:
             zfile.close()

spacetelescope/PyFITS

pyfits/file.py
@@ -77,7 +77,7 @@ class _File(object):
         memmap = True if memmap is None else memmap
 
         if fileobj is None:
-            self.__file = None
+            self._file = None
             self.closed = False
             self.binary = True
             self.mode = mode
@@ -123,13 +123,15 @@ class _File(object):
         self.readonly = False
         self.writeonly = False
 
-        # Initialize the internal self.__file object
+        # Initialize the internal self._file object
         if _is_random_access_file_backed(fileobj):
             self._open_fileobj(fileobj, mode, clobber)
         elif isinstance(fileobj, string_types):
             self._open_filename(fileobj, mode, clobber)
         else:
             self._open_filelike(fileobj, mode, clobber)
+
+        self.fileobj_mode = fileobj_mode(self._file)
 
         if isinstance(fileobj, gzip.GzipFile):
             self.compression = 'gzip'
@@ -147,18 +149,18 @@ class _File(object):
         # For 'ab+' mode, the pointer is at the end after the open in
         # Linux, but is at the beginning in Solaris.
         if (mode == 'ostream' or self.compression or
-            not hasattr(self.__file, 'seek')):
+            not hasattr(self._file, 'seek')):
             # For output stream start with a truncated file.
             # For compressed files we can't really guess at the size
             self.size = 0
         else:
-            pos = self.__file.tell()
-            self.__file.seek(0, 2)
-            self.size = self.__file.tell()
-            self.__file.seek(pos)
+            pos = self._file.tell()
+            self._file.seek(0, 2)
+            self.size = self._file.tell()
+            self._file.seek(pos)
 
         if self.memmap:
-            if not isfile(self.__file):
+            if not isfile(self._file):
                 self.memmap = False
             elif not self.readonly and not self._test_mmap():
                 # Test mmap.flush--see
@@ -167,7 +169,7 @@ class _File(object):
 
     def __repr__(self):
         return '<%s.%s %s>' % (self.__module__, self.__class__.__name__,
-                               self.__file)
+                               self._file)
 
     # Support the 'with' statement
     def __enter__(self):
@@ -179,13 +181,13 @@ class _File(object):
     def readable(self):
         if self.writeonly:
             return False
-        return isreadable(self.__file)
+        return isreadable(self._file)
 
     def read(self, size=None):
-        if not hasattr(self.__file, 'read'):
+        if not hasattr(self._file, 'read'):
             raise EOFError
         try:
-            return self.__file.read(size)
+            return self._file.read(size)
         except IOError:
             # On some versions of Python, it appears, GzipFile will raise an
             # IOError if you try to read past its end (as opposed to just
@@ -204,7 +206,7 @@ class _File(object):
         it's provided for compatibility.
         """
 
-        if not hasattr(self.__file, 'read'):
+        if not hasattr(self._file, 'read'):
             raise EOFError
 
         if not isinstance(dtype, np.dtype):
@@ -234,26 +236,26 @@ class _File(object):
             shape = (1,)
 
         if self.memmap:
-            return Memmap(self.__file, offset=offset,
+            return Memmap(self._file, offset=offset,
                           mode=MEMMAP_MODES[self.mode], dtype=dtype,
                           shape=shape).view(np.ndarray)
         else:
             count = reduce(lambda x, y: x * y, shape)
-            pos = self.__file.tell()
-            self.__file.seek(offset)
-            data = _array_from_file(self.__file, dtype, count, '')
+            pos = self._file.tell()
+            self._file.seek(offset)
+            data = _array_from_file(self._file, dtype, count, '')
             data.shape = shape
-            self.__file.seek(pos)
+            self._file.seek(pos)
             return data
 
     def writable(self):
         if self.readonly:
             return False
-        return iswritable(self.__file)
+        return iswritable(self._file)
 
     def write(self, string):
-        if hasattr(self.__file, 'write'):
-            _write_string(self.__file, string)
+        if hasattr(self._file, 'write'):
+            _write_string(self._file, string)
 
     def writearray(self, array):
         """
@@ -263,51 +265,51 @@ class _File(object):
         the file on disk reflects the data written.
         """
 
-        if hasattr(self.__file, 'write'):
-            _array_to_file(array, self.__file)
+        if hasattr(self._file, 'write'):
+            _array_to_file(array, self._file)
 
     def flush(self):
-        if hasattr(self.__file, 'flush'):
-            self.__file.flush()
+        if hasattr(self._file, 'flush'):
+            self._file.flush()
 
     def seek(self, offset, whence=0):
         # In newer Python versions, GzipFiles support the whence argument, but
         # I don't think it was added until 2.6; instead of assuming it's
         # present, we implement our own support for it here
-        if not hasattr(self.__file, 'seek'):
+        if not hasattr(self._file, 'seek'):
             return
-        if isinstance(self.__file, gzip.GzipFile):
+        if isinstance(self._file, gzip.GzipFile):
             if whence:
                 if whence == 1:
-                    offset = self.__file.offset + offset
+                    offset = self._file.offset + offset
                 else:
                     raise ValueError('Seek from end not supported')
-            self.__file.seek(offset)
+            self._file.seek(offset)
         else:
-            self.__file.seek(offset, whence)
+            self._file.seek(offset, whence)
 
-        pos = self.__file.tell()
+        pos = self._file.tell()
         if self.size and pos > self.size:
             warnings.warn('File may have been truncated: actual file length '
                           '(%i) is smaller than the expected size (%i)' %
                           (self.size, pos))
 
     def tell(self):
-        if not hasattr(self.__file, 'tell'):
+        if not hasattr(self._file, 'tell'):
             raise EOFError
-        return self.__file.tell()
+        return self._file.tell()
 
     def truncate(self, size=None):
-        if hasattr(self.__file, 'truncate'):
-            self.__file.truncate(size)
+        if hasattr(self._file, 'truncate'):
+            self._file.truncate(size)
 
     def close(self):
         """
         Close the 'physical' FITS file.
         """
 
-        if hasattr(self.__file, 'close'):
-            self.__file.close()
+        if hasattr(self._file, 'close'):
+            self._file.close()
 
         self.closed = True
 
@@ -357,16 +359,16 @@ class _File(object):
                 raise ValueError(
                     "Mode argument '%s' does not match mode of the input "
                     "file (%s)." % (mode, fmode))
-            self.__file = fileobj
+            self._file = fileobj
         elif isfile(fileobj):
-            self.__file = fileobj_open(self.name, PYFITS_MODES[mode])
+            self._file = fileobj_open(self.name, PYFITS_MODES[mode])
         else:
-            self.__file = gzip.open(self.name, PYFITS_MODES[mode])
+            self._file = gzip.open(self.name, PYFITS_MODES[mode])
 
         if fmode == 'ab+':
             # Return to the beginning of the file--in Python 3 when opening in
             # append mode the file pointer is at the end of the file
-            self.__file.seek(0)
+            self._file.seek(0)
 
     def _open_filelike(self, fileobj, mode, clobber):
         """Open a FITS file from a file-like object, i.e. one that has
@@ -374,7 +376,7 @@ class _File(object):
         """
 
         self.file_like = True
-        self.__file = fileobj
+        self._file = fileobj
 
         if fileobj_closed(fileobj):
             raise IOError("Cannot read from/write to a closed file-like "
@@ -382,15 +384,15 @@ class _File(object):
 
         if isinstance(fileobj, zipfile.ZipFile):
             self._open_zipfile(fileobj, mode)
-            self.__file.seek(0)
+            self._file.seek(0)
             # We can bypass any additional checks at this point since now
-            # self.__file points to the temp file extracted from the zip
+            # self._file points to the temp file extracted from the zip
             return
 
         # If there is not seek or tell methods then set the mode to
         # output streaming.
-        if (not hasattr(self.__file, 'seek') or
-            not hasattr(self.__file, 'tell')):
+        if (not hasattr(self._file, 'seek') or
+            not hasattr(self._file, 'tell')):
             self.mode = mode = 'ostream'
 
         if mode == 'ostream':
@@ -398,13 +400,13 @@ class _File(object):
 
         # Any "writeable" mode requires a write() method on the file object
         if (self.mode in ('update', 'append', 'ostream') and
-            not hasattr(self.__file, 'write')):
+            not hasattr(self._file, 'write')):
             raise IOError("File-like object does not have a 'write' "
                           "method, required for mode '%s'."
                           % self.mode)
 
         # Any mode except for 'ostream' requires readability
-        if self.mode != 'ostream' and not hasattr(self.__file, 'read'):
+        if self.mode != 'ostream' and not hasattr(self._file, 'read'):
             raise IOError("File-like object does not have a 'read' "
                           "method, required for mode %r."
                           % self.mode)
@@ -425,15 +427,15 @@ class _File(object):
 
         if ext == '.gz' or magic.startswith(GZIP_MAGIC):
             # Handle gzip files
-            self.__file = gzip.open(self.name, PYFITS_MODES[mode])
+            self._file = gzip.open(self.name, PYFITS_MODES[mode])
             self.compression = 'gzip'
         elif ext == '.zip' or magic.startswith(PKZIP_MAGIC):
             # Handle zip files
             self._open_zipfile(self.name, mode)
         else:
-            self.__file = fileobj_open(self.name, PYFITS_MODES[mode])
+            self._file = fileobj_open(self.name, PYFITS_MODES[mode])
             # Make certain we're back at the beginning of the file
-        self.__file.seek(0)
+        self._file.seek(0)
 
     def _open_zipfile(self, fileobj, mode):
         """Limited support for zipfile.ZipFile objects containing a single
@@ -457,8 +459,8 @@ class _File(object):
         if len(namelist) != 1:
             raise IOError(
               "Zip files with multiple members are not supported.")
-        self.__file = tempfile.NamedTemporaryFile(suffix='.fits')
-        self.__file.write(zfile.read(namelist[0]))
+        self._file = tempfile.NamedTemporaryFile(suffix='.fits')
+        self._file.write(zfile.read(namelist[0]))
 
         if close:
             zfile.close()