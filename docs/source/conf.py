8d3894e44216805e9c83399c2fd1eef621425e02
Erik M. Bray <embray@stsci.edu>
2016-01-29 10:40:21 -0500

    Forgot to update the conf.py for the docs before releasing; oh well

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -91,7 +91,7 @@ authors = [
     'Michael Droettboom',
     'Erik M. Bray'
 ]
-copyright_year = '2015'
+copyright_year = '2016'
 copyright = ', '.join([copyright_year] + authors)
 
 # The version info for the project you're documenting, acts as replacement for
@@ -99,9 +99,9 @@ copyright = ', '.join([copyright_year] + authors)
 # built documents.
 #
 # The short X.Y version.
-version = '3.4.dev'
+version = '3.4'
 # The full version, including alpha/beta/rc tags.
-release = '3.4.dev'
+release = '3.4'
 
 # !!!!!
 
